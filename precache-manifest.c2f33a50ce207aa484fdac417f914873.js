838f979b6919c9f7940201d71644f85c4d3b709e
Administrator <254412246@qq.com>
2020-08-15 01:15:56 +0800

    deploy

---
+++ precache-manifest.c2f33a50ce207aa484fdac417f914873.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a836a772d09aa1bb8eb8",
+    "url": "/morney-website/css/app.b041f87e.css"
+  },
+  {
+    "revision": "2bd58f5995f938d28f0af114aa5ae3c2",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "a836a772d09aa1bb8eb8",
+    "url": "/morney-website/js/app.19c6263e.js"
+  },
+  {
+    "revision": "73db29944fe28f1e2dac",
+    "url": "/morney-website/js/chunk-vendors.6fbbca9a.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/morney-website/robots.txt"
+  }
+]);
